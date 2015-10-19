744bfb5bb4db04911fb8dde0527edc6803cac236
Alex Slater <alex.slater.dev@googlemail.com>
2015-10-19 14:26:24 +0100

    Remove scroll bars

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,6 +1,8 @@
 (load-theme 'wombat t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
+
 ;;Start fullscreen
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
