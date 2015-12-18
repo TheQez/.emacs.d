d8f0343664e0dd3c44eb2ff481e56164630e6fa4
Alex Slater <alex.slater.dev@googlemail.com>
2015-12-18 16:49:06 +0000

    Add sequences and colours for Homework.org

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,4 +2,22 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done 1)
+
+(setq org-todo-keywords
+      '((sequence "TODO" "|" "DONE")
+	(sequence "NOTE" "INPRO" "CHECK" "PRINT" "|" "DONE")
+	(sequence "FIX" "|" "DONE")))
+
+(setq org-todo-keyword-faces
+      '(("TODO" . "firebrick2")
+	
+	("NOTE" . "firebrick2")
+	("INPRO" . "chocolate2")
+	("CHECK" . "gold2")
+	("PRINT" . "SpringGreen3")
+
+	("FIX" . "firebrick2")
+	
+	("DONE" . "DodgerBlue1")))
+
 (provide 'init-org)
