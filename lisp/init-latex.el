5c7dd3c650cae605754a4287b7c82a3b17e6b1ec
TheQez <alex.slater.dev@googlemail.com>
2018-09-10 00:46:53 +0100

    Set doc-view-ghostscript-program correctly on windows

--- lisp/init-latex.el
+++ lisp/init-latex.el
@@ -3,4 +3,9 @@
 
 (latex-preview-pane-enable)
 
+; Set where to find ghostscript if on windows
+(if (eq system-type 'windows-nt)
+	(setq doc-view-ghostscript-program (car (file-expand-wildcards "C:/Program Files/gs/gs*/bin/gswin64.exe")))
+)
+
 (provide 'init-latex)
