3838261e62f84e1d020f9a38c45a2e81796108bd
TheQez <alex.slater.dev@googlemail.com>
2018-09-10 18:49:10 +0100

    Disable Evil cursor in doc-view-mode

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -1,4 +1,11 @@
 (require 'evil)
 (evil-mode 1)
 
+; Evil cursor causes flickering in doc-view-mode, so disable it
+(evil-set-initial-state 'doc-view-mode 'emacs)
+(add-hook 'doc-view-mode-hook
+	  (lambda()
+	    (if (eq major-mode 'doc-view-mode)
+		    (set (make-local-variable 'evil-emacs-state-cursor) (list nil)))))
+
 (provide 'init-evil)
